91730f1abc4acf767b8fd3460c24ce42690e016e
Charlie <charliesun525@gmail.com>
2024-12-19 00:31:54 +0800

    创建 merlin.js

---
+++ merlin.js
@@ -0,0 +1,54 @@
+/*************************************
+
+项目名称：Merlin
+下载地址：https://arcane.getmerlin.in
+更新日期：2024-01-13
+脚本作者：𝓒𝓱𝓪𝓻𝓵𝓲𝓮
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/arcane\.getmerlin\.in\/v1\/user\/status url script-response-body https://raw.githubusercontent.com/Charlies214/Script/refs/heads/master/merlin.js
+
+[mitm]
+hostname = arcane.getmerlin.in
+
+*************************************/
+
+var Charlie = JSON.parse($response.body);
+
+Charlie.data.cost = {
+  "dailyUsage": {
+    "usage": 0,
+    "limit": 99999,
+    "resetsAt": 4092599349000
+  },
+  "monthlyUsage": {
+    "usage": 0,
+    "limit": 99999,
+    "resetsAt": 4092599349000
+  }
+};
+
+Charlie.data.features = {
+  "webImageGenerationTool": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "aiDetection": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "webPdfChatMessages": {"usage": 0, "limit": 99999999, "resetsAt": 4092599349000},
+  "aiPlagiarismChecker": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "ytSummary": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "fluxAIImageGenerator": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "aiResumeBuilder": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "aiHumanizer": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "projects": {"usage": 0, "limit": 99999999, "resetsAt": 4092599349000},
+  "wallflower": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "merlin": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "realtimeSerp": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "essayWriterV2": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "webImageGeneration": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "babyFaceGenerator": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000},
+  "attachments": {"usage": 0, "limit": 99999999, "resetsAt": 4092599349000},
+  "webPdfChat": {"usage": 0, "limit": 99999, "resetsAt": 4092599349000}
+};
+
+$done({body: JSON.stringify(Charlie)});
