e207b849a17b7b03dfec2f7a539da2a0bf71ce9d
Charlie <charliesun525@gmail.com>
2024-12-18 17:50:43 +0800

    更新 solves.js

--- solves.js
+++ solves.js
@@ -1,6 +1,6 @@
 /*************************************
 项目名称：Solves
-脚本功能：Solves解锁
+脚本功能：解锁会员
 脚本作者：𝓒𝓱𝓪𝓻𝓵𝓲𝓮
 更新时间：2023-12-20
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -15,27 +15,17 @@ hostname = olegbarinov.me
 *************************************/
 
 try {
-    // 解析响应体
     var chxm1023 = JSON.parse($response.body);
     
-    // 确保data对象存在
-    if (!chxm1023.data) {
-        chxm1023.data = {};
-    }
+    // 修改关键参数
+    chxm1023.areDonationsEnabled = true;
+    chxm1023.isUpgradeTrialAvailable = true;
+    chxm1023.upgradeTrialDuration = "999d";
+    chxm1023.upgradeBadgeMinIntervalWhenPreferred = "999999d";
+    chxm1023.upgradeBadgeMinIntervalWhenNotPreferred = "999999d";
     
-    // 修改响应数据
-    chxm1023.data = {
-        ...chxm1023.data,
-        "areDonationsEnabled": true
-    };
-    
-    // 发送修改后的数据
-    $done({
-        body: JSON.stringify(chxm1023)
-    });
-    
+    $done({body: JSON.stringify(chxm1023)});
 } catch (err) {
-    // 错误处理
     console.log('脚本运行出错: ' + err);
     $done({});
 }
