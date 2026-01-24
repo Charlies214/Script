d4023627afc63bf2fac8853abd6757592b61a76f
Charlie <charliesun525@gmail.com>
2026-01-24 19:20:19 +0800

    Update URL in yyyj.js to use 'master' branch

--- yyyj.js
+++ yyyj.js
@@ -1,6 +1,6 @@
 /*************************************
 [rewrite_local]
-^https?:\/\/lcapi\.engcorner\.cn\/1.1\/users\/5fba3b883a05a407c48d5d82 url script-response-body https://raw.githubusercontent.com/Charlies214/Script/main/yyyj.js
+^https?:\/\/lcapi\.engcorner\.cn\/1.1\/users\/5fba3b883a05a407c48d5d82 url script-response-body https://raw.githubusercontent.com/Charlies214/Script/master/yyyj.js
 
 [mitm]
 hostname = lcapi.engcorner.cn
